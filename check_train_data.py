fd6518fce280039f18055584d671ca2f686cbcb1
zuoshifan <zuoshifan@163.com>
2021-05-12 17:23:29 +0800

    Changes in check_train_data

--- check_train_data.py
+++ check_train_data.py
@@ -15,8 +15,8 @@ input_dir = './train_data'
 
 # ii = 300
 for ii in range(0, 1000, 99):
-    with h5py.File(input_dir + '/train_data.hdf5', 'r') as f:
-    # with h5py.File(input_dir + '/train_data_zero_mean.hdf5', 'r') as f:
+    # with h5py.File(input_dir + '/train_data.hdf5', 'r') as f:
+    with h5py.File(input_dir + '/train_data_zero_mean.hdf5', 'r') as f:
         in_map = f['input'][ii]
         rec_map = f['reconstruction'][ii]
 
@@ -26,12 +26,12 @@ for ii in range(0, 1000, 99):
 
     plt.figure(figsize=(13, 5))
     plt.subplot(121)
-    # plt.imshow(in_map, origin='lower', aspect='equal', vmin=0, vmax=3)
-    plt.imshow(in_map, origin='lower', aspect='equal', vmin=0, vmax=7)
+    plt.imshow(in_map, origin='lower', aspect='equal', vmin=0, vmax=3)
+    # plt.imshow(in_map, origin='lower', aspect='equal', vmin=0, vmax=7)
     plt.colorbar()
     plt.subplot(122)
     plt.imshow(rec_map, origin='lower', aspect='equal', vmin=0, vmax=3)
     plt.colorbar()
-    plt.savefig(input_dir + '/train_%04d.png' % ii)
-    # plt.savefig(input_dir + '/train_zero_mean_%04d.png' % ii)
+    # plt.savefig(input_dir + '/train_%04d.png' % ii)
+    plt.savefig(input_dir + '/train_zero_mean_%04d.png' % ii)
     plt.close()
